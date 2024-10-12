cc05c87812cd3074f6989dbdb7fb9aea38f736fa
Toxa1704 <Toxa1704@outlook.com>
2024-10-12 16:37:00 +0300

    main

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,14 @@
-const mainPageText = document.querySelector('.main-page-text');
-const label = document.querySelector('.menu-btn')
-const links = document.querySelector('.page-links')
-console.log(label, mainPageText,links);
+const mainPageText = document.querySelector(".main-page-text");
+const label = document.querySelector(".menu-btn");
+const links = document.querySelector(".page-links");
+console.log(label, mainPageText, links);
 
-
-label.addEventListener('click', () => {
-    if (mainPageText.style.display === 'none' && links.style.display === 'none') {
-        mainPageText.style.display = 'block';
-        links.style.display = 'flex';
-
-    } else {
-        mainPageText.style.display = 'none';
-        links.style.display = 'none';
-    }
-})
+label.addEventListener("click", () => {
+  if (mainPageText.style.display === "none" && links.style.display === "none") {
+    mainPageText.style.display = "block";
+    links.style.display = "flex";
+  } else {
+    mainPageText.style.display = "none";
+    links.style.display = "none";
+  }
+});

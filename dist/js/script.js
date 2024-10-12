c5bc724523a25d10c8edac11e65a68c3764a4ff9
Toxa1704 <Toxa1704@outlook.com>
2024-10-12 16:33:44 +0300

    main

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+const mainPageText = document.querySelector('.main-page-text');
+const label = document.querySelector('.menu-btn')
+const links = document.querySelector('.page-links')
+console.log(label, mainPageText,links);
+
+
+label.addEventListener('click', () => {
+    if (mainPageText.style.display === 'none' && links.style.display === 'none') {
+        mainPageText.style.display = 'block';
+        links.style.display = 'flex';
+
+    } else {
+        mainPageText.style.display = 'none';
+        links.style.display = 'none';
+    }
+})
